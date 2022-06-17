8712ccd962f21f3a310213960ff8da876201f7c9
DaniilPetrov <dany_petrov@icloud.com>
2022-06-17 17:54:27 +0300

    Setup App Logic

--- HW2.2 TrafficLights/ViewController.swift
+++ HW2.2 TrafficLights/ViewController.swift
@@ -9,10 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var trafficLights: [UIView]!
+    @IBOutlet var changeColorButton: UIButton!
+    private var nextColor = "red"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setDefaultLights()
+        changeColorButton.layer.cornerRadius = 15
+        changeColorButton.setTitle("Start", for: .normal)
     }
+    
 
+    
+    
+    
+    private func setDefaultLights() {
+        trafficLights.forEach{
+            $0.alpha = 0.3
+        }
+    }
+    @IBAction private func changeColorTapped(_ sender: UIButton) {
+        changeColorButton.setTitle("Next", for: .normal)
+        setDefaultLights()
+        switch nextColor {
+        case "red":
+            trafficLights[0].alpha = 1
+            nextColor = "yellow"
+        case "yellow":
+            trafficLights[1].alpha = 1
+            nextColor = "green"
+        case "green":
+            trafficLights[2].alpha = 1
+            nextColor = "red"
+        default: print("Error")
+        }
+    }
+    
 }
 
