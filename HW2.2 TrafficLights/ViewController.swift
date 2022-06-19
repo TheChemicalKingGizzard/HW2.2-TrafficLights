2f6d9eced31b8b79d99b9b4d44dec4056c9b84e0
DaniilPetrov <dany_petrov@icloud.com>
2022-06-19 12:52:34 +0300

    Small naming fix

--- HW2.2 TrafficLights/ViewController.swift
+++ HW2.2 TrafficLights/ViewController.swift
@@ -9,19 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var trafficLights: [UIView]!
+    @IBOutlet var trafficLightsViews: [UIView]!
     @IBOutlet var changeColorButton: UIButton!
-    private var nextColor = "red"
+    private var lightCounter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setDefaultLights()
         changeColorButton.layer.cornerRadius = 15
-        trafficLights.forEach{$0.layer.cornerRadius = 50}
+        trafficLightsViews.forEach{$0.layer.cornerRadius = $0.frame.width / 2}
     }
     
     private func setDefaultLights() {
-        trafficLights.forEach{
+        trafficLightsViews.forEach{
             $0.alpha = 0.3
         }
     }
@@ -39,17 +39,17 @@ class ViewController: UIViewController {
         changeColorButton.configuration = setupButton(with: "Next")
         setDefaultLights()
         
-        switch nextColor {
-        case "red":
-            trafficLights[0].alpha = 1
-            nextColor = "yellow"
-        case "yellow":
-            trafficLights[1].alpha = 1
-            nextColor = "green"
-        case "green":
-            trafficLights[2].alpha = 1
-            nextColor = "red"
-        default: print("Error in color changing")
+        switch lightCounter {
+        case 0:
+            trafficLightsViews[0].alpha = 1
+            lightCounter = 1
+        case 1:
+            trafficLightsViews[1].alpha = 1
+            lightCounter = 2
+        default:
+            trafficLightsViews[2].alpha = 1
+            lightCounter = 0
+        
         }
     }
 }
